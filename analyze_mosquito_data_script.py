5a6a479dd3ce7ecbc33aa37b5c54e1977dfad909
Andrea Zonca <zonca@sdsc.edu>
2014-10-21 14:21:37 -0700

    get filename as argument and save outputs based on that

--- analyze_mosquito_data_script.py
+++ analyze_mosquito_data_script.py
@@ -1,13 +1,20 @@
+import sys
 import pandas as pd
 import analyze_mosquito_data_lib as mosquito_lib
 
-filename = "A1_mosquito_data.csv"
+filename = sys.argv[1]
+
+print "Analyzing", filename
 
 # read the data
 data = pd.read_csv(filename)
 data["temperature"] = mosquito_lib.fahr_to_celsius(
                               data["temperature"])
-parameters = mosquito_lib.analyze(data, "plot.png")
+							  
+print "Running analyze"
+parameters = mosquito_lib.analyze(data, 
+                    filename.replace("csv", "png"))
 
+print "Saving parameters"				
 # save parameters to file
-parameters.to_csv("parameters.csv")
+parameters.to_csv(filename.replace("data", "parameters"))
